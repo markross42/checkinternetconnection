519b182e6426b101459ef47710a88ad6d2411f5e
Mark Ross <mark@markross.de>
2019-10-16 23:42:51 +0200

    add some information to README

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# checkinternetconnection
-check internet connection by ping host and use resultcode and roundtrip time to descide, if the connection is up
+# check internet connection
+Check internet connection by one ping host and use resultcode and roundtrip time to descide, if the connection is up
+
+When the ping failes, the internet connection seems down. 
+A timestamp is written to a log file - start of failue.
+When the ping succeds afterwards, the internet connection seems to be up again.
+A timestamp is written to a log file - end of failue.
+
+When the ping roundtrip is less than (configurable): 10ms, my internal router answers the ping. 
+(My ISP does so).
+A timestamp is written to a log file - failue.
+
+## Operation systems
+The bash shell script runs on linux based os and Mac OSX.
+
+## Depencencies
+basic calulator and curl
+
+```
+sudo apt install bc
+sudo apt install curl
+```
+
+## Configuration
+In the script you find some variables, you could set - but you don´t need to.
+
+```
+HOST_TO_PING=8.8.8.8
+```
+This is the host for the ping. I configured google DNS (8.8.8.8). When this host is down and can´t respond. 
+I think, my internet connection is the lesser evil. 
+
+```
+LOG_FILE=checkinternetconnection.log
+```
+
+```
+MIN_TIME=10.0
+```
+Minimum ping time - my isp responds every ping. Even if the network is not
+reachable by the bradband router. So ping times less than 10ms are internal -> FAILURE
+
+
+```
+WAITING=5
+```
+How long - in seconds - should be waited between pings
---
+++ checkinternetconnection.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# check internet connection by ping host and use resultcode and roundtrip time to descide, if the connection is up
+#
+
+HOST_TO_PING=8.8.8.8 # 8.8.8.8 / 4.4.4.4 google DNS
+LOG_FILE=checkinternetconnection.log
+BC_CMD='/usr/bin/bc -l'
+#minimum ping time - my isp responds every ping. Even if the network is not
+# reachable by the bradband router. So ping times less than 10ms are internal -> FAIL
+MIN_TIME=10.0
+#how long - in seconds - should be waited between pings
+WAITING=5
+
+#init fault indicator (ping failed -> 1; ping ok  -> 0)
+FAILURE=0
+
+while true
+do
+  DATETIME=$(date +%Y.%m.%d\;%H:%M:%S)
+  #ping
+  PING_RESULT=$(ping -qc1 $HOST_TO_PING)
+  RESULT_CODE=$?
+  #grab only time from ping
+  TIME=$(echo -e "$PING_RESULT"  | tail -n1 | awk '{print $4}' | cut -f 2 -d "/")
+  if [ $RESULT_CODE -ge 1 ] && [ $FAILURE -eq 0 ] # the ping was successfull
+    then
+    FAILURE=1
+    echo "$DATETIME;Start failure;$TIME;PING_FAILED"
+    echo "$DATETIME;Start failure;$TIME;PING_FAILED" >> $LOG_FILE
+  fi
+  
+  if [ $RESULT_CODE -eq 0 ] && [ $FAILURE -eq 0 ] # the ping was successfull
+  then
+    #if (( $(echo "$TIME < $MIN_TIME" | $BC_CMD ) )) # was the ping in the internal or external network?
+    PING_TOO_QUICKLY=$(echo "$TIME < $MIN_TIME" | bc -l)
+    if [ $PING_TOO_QUICKLY -eq 1 ] # was the ping in the internal or external network?
+    then
+      FAILURE=1
+      echo "$DATETIME;Start failure;$TIME;TOO_QUICKLY"
+      echo "$DATETIME;Start failure;$TIME;TOO_QUICKLY" >> $LOG_FILE
+    else
+      FAILURE=0
+      echo "$DATETIME;Ping ok;$TIME;OK"
+    fi
+  fi
+
+  if [ $RESULT_CODE -eq 0 ] && [ $FAILURE -eq 1 ] # the ping was successfull, failure exits
+  then
+    if (( $(echo "$TIME < $MIN_TIME" |bc -l) )) # was the ping in the internal or external network
+    then
+      #FAILURE=1
+      echo "$DATETIME;Failure exists;$TIME;TOO_QUICKLY"
+      #echo $DATETIME ";Start failure;$TIME;TOO_QUICKLY" >> $LOG_FILE
+    else
+      FAILURE=0
+    echo "$DATETIME;End failure;$TIME;PING_OK"
+    echo "$DATETIME;End failure;$TIME;PING_OK" >> $LOG_FILE
+    fi
+  fi
+  sleep $WAITING
+done
