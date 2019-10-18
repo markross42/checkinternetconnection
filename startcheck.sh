003e358eddc70adff214e1c22be047559664b6b3
Mark Ross <mark@markross.de>
2019-10-18 22:47:59 +0200

    add startup script for crontab

--- startcheck.sh
+++ startcheck.sh
@@ -2,12 +2,17 @@
 #script for starting file from crontab, if script is running, do nothing
 # for example, run startcheck.sh all 5 minutes
 # */5 * * * * /path/to/startcheck.sh
+date
+GREP_PROCESS=checkinternetco
 FILE_NAME=checkinternetconnection.sh
 cd $(dirname $0)
-PGREP_RESULT=$(/usr/bin/pgrep $FILE_NAME)
+PGREP_RESULT=$(/usr/bin/pgrep $GREP_PROCESS)
 RESULT_CODE=$?
 if [ $RESULT_CODE -ge 1 ]
 then
   #start FILE_NAME in background
   nohup ./$FILE_NAME &
+  echo "started script, wasn´t running"
+else
+  echo "$FILE_NAME is running"
 fi
