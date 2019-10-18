d81abde9d3877c4c32a051977449de1bf87c4302
Mark Ross <mark@markross.de>
2019-10-18 22:29:04 +0200

    add startup script for crontab

--- startcheck.sh
+++ startcheck.sh
@@ -6,7 +6,7 @@ FILE_NAME=checkinternetconnection.sh
 cd $(dirname $0)
 PGREP_RESULT=$(/usr/bin/pgrep $FILE_NAME)
 RESULT_CODE=$?
-if [ RESULT_CODE -ge 1 ]
+if [ $RESULT_CODE -ge 1 ]
 then
   #start FILE_NAME in background
   nohup ./$FILE_NAME &
